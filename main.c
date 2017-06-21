9c066773302f2fc1beb18119fe9318e463ee6f2c
Virul-git <katlavirul.vk@gmail.com>
2017-06-21 19:25:11 +0000

    intterupt enabled

--- main.c
+++ main.c
@@ -2,9 +2,32 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+void scheduler()
+{
+
+}
+
+
 
 int main()
 {
+	sei(); 			// global interrupt elabled
 	
+	//////////////////  set for timer1 overflow interrupt
+	TCCR1B =0x01;
+	TIMSK = 0x07;
+
+	while(1)
+	{
+
+	}
 	return 0;
+}
+
+ISR(TIMER1_OVF_vect)
+{
+
+	scheduler();
+
+
 }
